9ebd1b8492f97980f9e786389eecd22086754c64
wgrabis <wgrabis@gmail.com>
2015-08-15 01:08:42 +0200

    add git ignore

--- src/Events/Executors/AppNameExecutor.scala
+++ src/Events/Executors/AppNameExecutor.scala
@@ -4,5 +4,5 @@ package Events.Executors
  * Created by Admin on 2015-08-14.
  */
 class AppNameExecutor extends Executor{
-
+  override def execute(): Unit = ???
 }
--- src/Events/Executors/GameButtonExecutor.scala
+++ src/Events/Executors/GameButtonExecutor.scala
@@ -4,5 +4,5 @@ package Events.Executors
  * Created by Admin on 2015-08-13.
  */
 class GameButtonExecutor(posX: Int, posY: Int) extends Executor{
-
+  override def execute(): Unit = ???
 }
